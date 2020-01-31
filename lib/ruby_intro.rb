d13cf1f55915d4f5f649f710ea8279c011ddd537
dina-mayett <dina.mayett@gmail.com>
2020-01-31 04:33:21 +0000

    part 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,53 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  else 
+    return arr.inject(:+)
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  else 
+    return arr.sort.last(2).inject(:+)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.empty?
+    return false
+  else
+    return arr.permutation(2).any?{|a,b| a + b == n}
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  elsif (s[0].downcase =~ /[b-df-hj-np-tv-xz]/)
+    return true
+  else 
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /[^01]/ || s.empty?
+    return false
+  elsif s.to_i % 4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
