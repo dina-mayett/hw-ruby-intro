6f1d52c9f32dcf46a9ab249d72fe390307477917
dina-mayett <dina.mayett@gmail.com>
2020-02-01 02:18:36 +0000

    hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -55,5 +55,15 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%0.2f" % @price
+  end
 end
